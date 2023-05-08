8aa1451d36f8e8e935dd2cb32180e4f041f19c49
Reza Iskandar <rpi75@yahoo.com>
2023-05-07 22:32:04 -0500

    Completed the exercise

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,11 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
-        return "Food";
+        if (animal.equals("Cat"))
+            return (isPremium) ? "Salmon" : "Tuna";
+        else if (animal.equals("Hamster"))
+            return "Cabbage";
+
+        return "Dog Food";
     }
 }
